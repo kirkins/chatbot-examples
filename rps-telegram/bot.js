384c350604083531d44f6c48bc2c80870ebb6495
Philip Kirkbride <kirkins@gmail.com>
2018-10-04 16:31:08 -0400

    add game choice to response

--- rps-telegram/bot.js
+++ rps-telegram/bot.js
@@ -1,4 +1,3 @@
-const excuse = require('huh');
 const botBuilder = require('claudia-bot-builder');
 const telegramTemplate = botBuilder.telegramTemplate;
 
@@ -12,21 +11,17 @@ module.exports = botBuilder(message => {
     if (gameChoices.includes(message.text.toLowerCase())) {
       var userChoice = gameChoices.indexOf(message.text.toLowerCase());
       var botChoice = Math.floor(Math.random() * 3);
-      if (userChoice == botChoice) return 'tie';
-      else if (userChoice == 0 && botChoice == 1) return "lose"
-      else if (userChoice == 1 && botChoice == 2) return "lose"
-      else if (userChoice == 2 && botChoice == 0) return "lose"
-      else if (userChoice == 0 && botChoice == 2) return "win"
-      else if (userChoice == 1 && botChoice == 0) return "win"
-      else if (userChoice == 2 && botChoice == 1) return "win"
+      if (userChoice == botChoice) return gameChoices[botChoice] +', tie';
+      else if (userChoice == 0 && botChoice == 1) return gameChoices[botChoice] + ", you lose"
+      else if (userChoice == 1 && botChoice == 2) return gameChoices[botChoice] + ", you lose"
+      else if (userChoice == 2 && botChoice == 0) return gameChoices[botChoice] + ", you lose"
+      else if (userChoice == 0 && botChoice == 2) return gameChoices[botChoice] + ", you win"
+      else if (userChoice == 1 && botChoice == 0) return gameChoices[botChoice] + ", you win"
+      else if (userChoice == 2 && botChoice == 1) return gameChoices[botChoice] + ", you win"
     } else {
       return new telegramTemplate.Text("hi " + message.text)
         .addReplyKeyboard([gameChoices])
         .get();
     }
-  else
-    return 'Thanks for sending ' + request.text  + 
-      '. Your message is very important to us, but ' + 
-      excuse.get();
 });
 
